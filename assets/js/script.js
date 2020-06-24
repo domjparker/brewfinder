cec10f9e737c6dfab9b432788d5e2d3a2da10152
Rettus code <michael.rettusii@gmail.com>
2020-06-24 13:37:54 -0700

    completed extraction of minimum needed data from pubs and built useable array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-
+    //array to store brewery data objects
+    var breweries = [];
+    //replace with UI from front end selector
     var city = prompt("Enter City Name")
     var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city;
 
@@ -7,7 +9,19 @@ $(document).ready(function() {
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+        //remove console log before final release
         console.log(response);
+        //iterates through array of breweries creating objects and filling array for program use
+        for (var i=0; i < 20; i++){
+            var breweryData = {
+            }
+            breweryData.name = response[i].name;
+            breweryData.lat = response[i].latitude;
+            breweryData.lon = response[i].longitude;
+            breweries.push(breweryData);
+        }
+        //remove console log before final release
+        console.log(breweries);
       })
 
 
